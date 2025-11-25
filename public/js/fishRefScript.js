46b40aa60faad63cbd5c7c6c92f2a1b148f40175
Christian <christiandennis67@gmail.com>
2025-11-24 19:44:52 -0500

    Add fish-ref.html tab clear and content loading with temporary clear button

--- public/js/fishRefScript.js
+++ public/js/fishRefScript.js
@@ -2,6 +2,7 @@
 const getFishBtn = document.getElementById('getFishBtn');
 const getTackleBtn = document.getElementById('getTackleBtn');
 const getCondBtn = document.getElementById('getCondBtn');
+const clearBtn = document.getElementById('clearBtn');
 
 const catContents = document.getElementById('catContents');
 
@@ -27,26 +28,45 @@ const resetCatContents = () => {
 getFishData();
 resetCatContents();
 
+clearBtn.addEventListener('click', () => {
+    resetCatContents();
+    console.clear();
+    console.log("Contents cleared");
+});
+
 getFishBtn.addEventListener('click', () => {
-    console.log("Button clicked");
+    console.log("Button 1 clicked");
+    resetCatContents();
     for (let i = 0; i < fishData.length; i++) {
         const fishCard = document.createElement('div');
         fishCard.className = 'fish-card';
-
-        const catContents = document.getElementById('catContents');
         catContents.append(fishCard);
 
         const h3 = document.createElement('h3');
         h3.innerText = `${fishData[i].commonName}`;
         fishCard.append(h3);
 
-        const p = document.createElement('p');
-        let keyValue;
-        // Here, I want to iterate over the properties of Obj[i] and 
-        // fishCard.append `<p><strong>${Obj[i].key[k]}</strong>: ${Obj[i].value[k]}</p>`
-        // 
-        // for (let k = 2; k < Object.values(fishData[i]).length; k++) {
-        //     console.log(fishData(i).??????)
-        // }
+        let results = Object.keys(fishData[i]).map(key => {
+            console.log(key, ': ', fishData[i][key]);
+            return `<p><strong>${key}</strong>: ${fishData[i][key]}</p>`;
+        });
+        fishCard.insertAdjacentHTML('beforeend', results.join(''));
     }
 });
+
+getTackleBtn.addEventListener('click', () => {
+    console.log("Button 2 clicked");
+    resetCatContents();
+    const p = document.createElement('p');
+    p.innerText = "Lorem ipsum dolor sit, amet consectetur adipisicing elit.";
+    catContents.append(p);
+})
+
+getCondBtn.addEventListener('click', () => {
+    console.log("Button 3 clicked");
+    resetCatContents();
+    const p = document.createElement('p');
+    p.innerText = "Cupiditate soluta hic non aut molestias ullam sequi, molestiae eveniet dolor voluptatum aspernatur veniam pariatur exercitationem maiores ipsam deleniti quisquam quae?";
+    catContents.append(p);
+})
+
